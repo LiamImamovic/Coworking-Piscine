5745929382eecf872c576b80e955beddc7dba86f
LiamImamovic <liamimamovic1@gmail.com>
2023-04-04 15:01:17 +0200

    Add Middlewares

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var morgan = require('morgan')
+var favicon = require('serve-favicon')
+
 
 const coworkings = require('./mock-coworkings');
+
+app.use(morgan('dev'));
+app.use(favicon((__dirname,'favicon.ico')))
 
 app.get('/api/coworkings', (req, res) => {
     // Renvoyer tous les coworkings au format json, uniquement ceux dont la surface est supérieure à 500
@@ -14,13 +20,13 @@ app.get('/api/coworkings', (req, res) => {
 })
 
 app.get('/api/coworkings/:id', (req, res) => {
-    // Afficher le nom du coworking qui correspond à l'id en paramètre
-    const myCoworking = coworkings.find((coworking) => { return coworking.id === Number(req.params.id) })
+
+    const myCoworking = coworkings.find(element => element.id == req.params.id)
     const msg = `Le coworking  n°${myCoworking.id} a bien été retourné`
-    res.json({message: msg, data: myCoworking})
+
+    myCoworking ? res.json({message: msg, data: myCoworking}) : res.status(404).json({message: `Le coworking n°${myCoworking.id}'existe pas`})
+
 })
-
-
 
 app.listen(port, () => {
     console.log(`L'app sur le port ${port}`)
