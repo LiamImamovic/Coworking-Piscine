e94ea52c3f2f14bf286bb2ab32cfdfd4c78083ac
LiamImamovic <liamimamovic1@gmail.com>
2023-04-04 12:42:33 +0200

    new features

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const coworkings = require('./mock-coworkings');
+
+app.get('/api/coworkings', (req, res) => {
+    // Renvoyer tous les coworkings au format json, uniquement ceux dont la surface est supérieure à 500
+    console.log(req.query)
+    const limit = req.query.limit || 200
+    const result = coworkings.filter(element => element.superficy > limit);
+
+    res.json(result)
 })
 
+app.get('/api/coworkings/:id', (req, res) => {
+    // Afficher le nom du coworking qui correspond à l'id en paramètre
+    const myCoworking = coworkings.find((coworking) => { return coworking.id === Number(req.params.id) })
+    const msg = `Le coworking  n°${myCoworking.id} a bien été retourné`
+    res.json({message: msg, data: myCoworking})
+})
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`L'app sur le port ${port}`)
 })
